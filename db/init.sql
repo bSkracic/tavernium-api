7c7915e7c955b557f38a247f7809072181029ed7
Borna Skracic <bskracic@gmail.com>
2021-05-19 19:52:28 +0200

    revised psql campaign function

--- db/init.sql
+++ db/init.sql
@@ -43,27 +43,38 @@ create table if not exists messages(
     constraint fk_message_campaign foreign key (campaign_id) references campaigns(id)
 );
 
-create function set_avataruser(_uuid text,  _useriD text, _image text) 
-returns type integer
-as$$
-declare
-    _uuid text := $1
-    _userID text := $2
-    _image text := $3
-    target_user_id integer;
+create or replace function set_avataruser1(_uuid varchar(36),  _useriD varchar(36), _image text) 
+returns integer
+as
+$$
+    declare _uuid text := $1;
+    declare _userID text := $2;
+    declare _image text := $3;
+    declare target_user_id integer;
 begin
     select count(*) into target_user_id from images where user_id = _userID;
     if target_user_id > 0 then 
-        update images set image = _image where user_id = _userID
+        update images set image = _image where user_id = _userID;
     else
-        insert into images(uuid, user_id, image) values(_uuid, _userID, _image)
+        insert into images(uuid, user_id, image) values(_uuid, _userID, _image);
     end if;
 end;
-$$
+$$LANGUAGE plpgsql;
 
-select campaigns.id, campaigns.title, campaigns.thematics, users.username, count(campaigns_user.id) as joined
-from campaigns  
-inner join users on users.uuid = campaigns.user_starter_id 
-inner join campaigns_user on campaigns_user.user_id = '11885ffa-0a3e-46b3-82cf-2f18065629fc' and campaigns_user.campaign_id = campaigns.id
-where campaigns.title like 'dwa%'
-group by campaigns_user.id
+create or replace function get_campaignbyname(_term text, _user_id varchar(36))
+returns table (id int, title varchar(50), thematics varchar(50), username varchar(50), joined boolean)
+as
+$$
+declare term text := $1;
+declare userId varchar(36) := $2;
+begin
+    return query
+    select campaigns.id, campaigns.title, campaigns.thematics, users.username, 
+        case when campaigns_user.id is not null then true else false end joined
+    from campaigns  
+    inner join users on users.uuid = campaigns.user_starter_id  
+    full join campaigns_user on campaigns_user.user_id = userId and campaigns_user.campaign_id = campaigns.id
+    where campaigns.title like concat(term, '%');
+end;
+$$
+language plpgsql;
